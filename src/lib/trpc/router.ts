3973f4b3bbd615b13b81cf404ade415973087d95
Tin Nguyen <hopthucuatin@gmail.com>
2022-12-13 15:39:46 +0700

    fix: add bcrypt

--- src/lib/trpc/router.ts
+++ src/lib/trpc/router.ts
@@ -1,10 +1,8 @@
 import { auth } from './routes/auth';
-import delay from 'delay';
 import { t } from './t';
 
 export const router = t.router({
 	greeting: t.procedure.query(async () => {
-		await delay(500); // 👈 simulate an expensive operation
 		return `Hello tRPC v10 @ ${new Date().toLocaleTimeString()}`;
 	}),
 	auth: auth
