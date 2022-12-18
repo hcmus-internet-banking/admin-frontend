6bb3f4778981d9e888128fb01a02fa7de4956068
Tin Nguyen <hopthucuatin@gmail.com>
2022-12-18 18:37:03 +0700

    chore: add title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+	import { PUBLIC_SITE_NAME } from '$env/static/public';
 	import AdminManage from '$lib/components/home/adminManage.svelte';
 	import Deposit from '$lib/components/home/deposit.svelte';
 </script>
+
+<svelte:head>
+	<title>Admin Home - {PUBLIC_SITE_NAME}</title>
+</svelte:head>
 
 <div class="grid space-y-4">
 	<h1 class="text-sm">Welcome to SvelteKit</h1>
