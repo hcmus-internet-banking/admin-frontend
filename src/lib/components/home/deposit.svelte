42a659b94a4686aead1603954073c70e6a360d84
Tin Nguyen <hopthucuatin@gmail.com>
2022-12-18 18:04:20 +0700

    split fles

---
+++ src/lib/components/home/deposit.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import AppButton from '../appButton.svelte';
+	import AppInput from '../appInput.svelte';
+
+	let depositAmount: string;
+	let bankNumber: string;
+</script>
+
+<section class="rounded-md border border-green-300 [&>*]:p-4">
+	<h2 class="bg-green-200">Deposit</h2>
+
+	<div class="space-y-2">
+		<AppInput bind:value={bankNumber} placeholder="Bank number" />
+		<AppInput bind:value={depositAmount} placeholder="Deposit amount" />
+
+		<div class="h-1" />
+		<AppButton>Deposit</AppButton>
+	</div>
+</section>
